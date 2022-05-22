deb18d45c1923f965b6c0892a9562313552a04ba
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-05-22 15:44:59 +0400

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Nikoloz Chitashvili on 22.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
