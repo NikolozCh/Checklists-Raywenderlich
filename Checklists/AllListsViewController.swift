e4a45ab6166518a834b2771ace271c7c668af973
Nikoloz Chitashvili <chitashvili.nikoloz@gmail.com>
2022-05-28 22:05:10 +0400

    Finished the chapter

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
 
     let cellIdentifier: String = "ChecklistCell"
     let checklistSegueIdentifier: String = "ShowChecklist"
+    let editChecklistSegueID: String = "EditChecklist"
+    let addChecklistSegueID: String  = "AddChecklist"
     var lists: [Checklist] = [Checklist]()
     
     override func viewDidLoad() {
@@ -46,14 +48,57 @@ class AllListsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let checklist = lists[indexPath.row]
-        
         performSegue(withIdentifier: checklistSegueIdentifier, sender: checklist)
     }
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let checklist = lists[indexPath.row]
+        performSegue(withIdentifier: editChecklistSegueID, sender: checklist)
+    }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            lists.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    // MARK: - Segue prepare overridden method
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == checklistSegueIdentifier {
             let vController = segue.destination as! ChecklistViewController
             vController.checkList = sender as? Checklist
         }
+        else if segue.identifier == addChecklistSegueID {
+            let vController = segue.destination as! ListDetailViewController
+            vController.delegate = self
+        }
+        else if segue.identifier == editChecklistSegueID {
+            let vController = segue.destination as! ListDetailViewController
+            vController.delegate = self
+            vController.checklistToEdit = sender as? Checklist
+        }
+    }
+    
+    // MARK: - List detail protocol delegate methods
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishdAdding checklist: Checklist) {
+        let newItemIndex: Int = lists.count
+        lists.append(checklist)
+        let newItemIndexPath = IndexPath(row: newItemIndex, section: 0)
+        tableView.insertRows(at: [newItemIndexPath], with: .automatic)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
+        if let itemIndex = lists.firstIndex(of: checklist) {
+            print("Out item's new index: \(itemIndex)")
+            if let cell = tableView.cellForRow(at: IndexPath(item: itemIndex, section: 0)) {
+                cell.textLabel!.text = checklist.name
+            }
+        }
+        navigationController?.popViewController(animated: true)
     }
 }
--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class Checklist: NSObject {
+class Checklist: Equatable {
+    static func == (lhs: Checklist, rhs: Checklist) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
     var name: String
     
     init(listItemName name: String) {
         self.name = name
-        super.init()
+//        super.init()
     }
 }
